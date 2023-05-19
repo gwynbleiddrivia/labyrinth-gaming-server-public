34701e9643ebbf919e49796c0d43a810d0262573
gwynbleiddrivia <yasinzme2@gmail.com>
2023-05-20 00:56:30 +0600

    mongodb connected successfully, used environment variables, await connect is commented out

--- index.js
+++ index.js
@@ -3,13 +3,62 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
+
 //middlewares here
 app.use(cors())
 app.use(express.json())
+require('dotenv').config()
+
+
 
 app.get('/',(req,res)=>{
 	res.send('Labyrinth gaming server is running, testing...')
 })
+
+
+
+
+
+//MongoDB application code
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c32luun.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  //  await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+console.log(uri)
+
+
 
 app.listen(port, ()=>{
 	console.log(`Labyrinth gaming server is running on port: ${port}`)
